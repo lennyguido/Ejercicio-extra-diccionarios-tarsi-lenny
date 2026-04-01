b26a6c9b09f3dff6ecda43641df05d6b994a2e43
MATÍAS TARSI <50154645@ort>
2026-04-01 08:27:42 -0300

    aura

---
+++ entrevista.cs
@@ -0,0 +1,25 @@
+namespace Ej_extra_Tarsi_Lenny;
+public class entrevista
+{
+private string nombreCandidato;
+private datetime fechayhora;
+private int duracion;
+private string puesto;
+private string nombreEntrevistador;
+
+public entrevista entrevista(string nombreCandidato, datetime fechayhora, int  duracion, string puesto, string nombreEntrevistador ){
+
+
+this.nombreCandidato = nombreCandidato
+this.fechayhora = fechayhora;
+this.duracion = duracion;
+this.puesto = puesto;
+this.nombreEntrevistador = nombreEntrevistador
+
+
+}
+
+
+
+
+}
